bcfd2064de0313eaa5c7841ff7e7aa11c4fbf6c4
Daniel Armando Peña García <135689456+DanielPenaGarcia@users.noreply.github.com>
2024-10-02 13:03:03 -0700

    Conexión a sql

--- src/main.js
+++ src/main.js
@@ -1,72 +1,16 @@
-import { where } from "./query-builder/condition.builder.js";
-import { JoinTypes } from "./query-builder/query.js";
-import { ReviewRepository } from "./repositories/review.repository.js";
 import { TeacherRepository } from "./repositories/teacher.repository.js";
 
 const teacherRepository = new TeacherRepository();
 
-const result = await teacherRepository.find({
-  fields: ["id", "names", "email"],
-  limit: 10,
-  offset: 0,
-  joins: [
-    {
-      table: "classes",
-      type: JoinTypes.INNER,
-      field: "id",
-      fields: ["name", "description"],
-      fieldNameReference: "teacher_id",
-    },
-  ],
-});
+// const insert = await teacherRepository.create({
+//   fields: ['email', 'password', 'academic_id', 'photo', 'names', 'father_last_name', 'mother_last_name', 'curp'],  // Corregido el typo en 'password'
+//   values: [
+//       ['dapgpena@gmail.com', '1234', 1, '', 'David', 'Pena', 'Gonzalez', 'PEGD021110HSRXRNA6']
+//   ]
+// });
 
-console.log(result); //SELECT by FIND
+// console.log(insert);
 
-const result2 = await teacherRepository.findById(1, {
-  fields: ["id", "names", "email"],
-  limit: 10,
-  offset: 0,
-  joins: [
-    {
-      table: "classes",
-      type: JoinTypes.INNER,
-      field: "id",
-      fields: ["name", "description"],
-      fieldNameReference: "teacher_id",
-    },
-  ],
-});
+const select = await teacherRepository.findById(4, {fields: []});
 
-console.log(result2); //SELECT by ID
-
-const result3 = await teacherRepository.update({
-  setValues: { column: "names", value: "Daniel" },
-  conditions: where().equal("id", 1).build(),
-});
-
-console.log(result3); //UPDATE
-
-const result4 = await teacherRepository.delete({ conditions: where().equal("id", 1).build() });
-
-console.log(result4); //DELETE
-
-const result5 = await teacherRepository.create({ fields: ["names", "email"], values: [["Daniel", "dapgpena@gmail.com"]]});
-
-console.log(result5); //INSERT
-
-const reviewRepository = new ReviewRepository();
-
-const result6 = await reviewRepository.find({fields: ["id", "comment"], limit: 10, offset: 0});
-
-console.log(result6); //SELECT by FIND
-
-const result7 = await reviewRepository.findById(1, {fields: ["id", "comment"], limit: 10, offset: 0});
-
-console.log(result7); //SELECT by ID
-
-const result8 = await reviewRepository.update({setValues: {column: "comment", value: "Excelente"}, conditions: where().equal("id", 1).build()});
-console.log(result8); //UPDATE
-
-const result9 = await reviewRepository.delete({conditions: where().equal("id", 1).build()});
-
-console.log(result9); //DELETE
+console.log(select);
