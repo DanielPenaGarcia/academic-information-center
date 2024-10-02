5333c52e3f57c1116f578140854fd4e661a8ac65
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2024-10-02 16:51:12 -0700

    refactor

--- src/configs/database.config.js
+++ src/configs/database.config.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 export const connection = await mysql.createConnection({
     user: 'root',
-    password: 'bone770115',
+    password: '1234',
     database: 'academic_information_center',
     host: 'localhost',
     port: 3306
