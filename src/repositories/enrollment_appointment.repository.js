b0ed48cd1a0ea8d4e08c9988252586e12b0005b6
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2024-10-02 16:48:41 -0700

    refactor

--- src/repositories/enrollment_appointment.repository.js
+++ src/repositories/enrollment_appointment.repository.js
@@ -1,12 +1,16 @@
+import { connection } from "../configs/database.config.js";
 import { where } from "../query-builder/condition.builder.js";
 import { QueryBuilder } from "../query-builder/query.builder.js";
 import { RepositoryStrategy, RepoStrategy } from "./repository-strategy/repository-strategy.js";
 
 export class EnrollmentAppointmentRepository extends RepositoryStrategy{
 
+    connection;
+
     constructor(){
         super();
         this.table = RepoStrategy.ENROLLMENT_APPOINTMENT;
+        this.connection = connection;
     }
 
     async find(object) {
@@ -20,44 +24,85 @@ export class EnrollmentAppointmentRepository extends RepositoryStrategy{
             query.conditions(object.conditions);
         }
         if (object.joins){
-            //Inners must be an array of objects
-            query.joinTable(object.joins);
+          //Joins must be an array of objects
+          object.joins.forEach(join => {
+            query.joinTable(join.table, join.type, join.field, join.fields, join.fields, join.fieldNameReference);
+        });
         }
         if (object.limit){
             //Limit must be a number
             query.limit(object.limit);
         }
-        if (object.limit && object.offset){
+        if (object.limit && object.offset >= 0){
             //Offset must be a number
             query.offset(object.offset);
         }
 
-        query.build();
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 
     async findById(id, object) {
-        let condition = where().equal('id', id);
+        let condition = where().equal(`${this.table}.id`, id).build();
         let query = QueryBuilder().select(this.table).conditions(condition);
-        console.log(query.build());
-        // Implement findById method
+        if (object.fields !== undefined) {
+            //Fields must be an array of strings or a string
+            query.fields(object.fields);
+        }
+        if (object.conditions){
+            //Conditions must be an array of objects
+            query.conditions(object.conditions);
+        }
+        if (object.joins){
+            //Joins must be an array of objects
+            object.joins.forEach(join => {
+                query.joinTable(join.table, join.type, join.field, join.fields, join.fields, join.fieldNameReference);
+            });
+        }
+        if (object.limit){
+            //Limit must be a number
+            query.limit(object.limit);
+        }
+        if (object.limit && object.offset >= 0){
+            //Offset must be a number
+            query.offset(object.offset);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result[0];  // Devuelve el resultado de la ejecución de la query
     }
 
     async create(object) {
-        // Implement create method
-        let query = QueryBuilder().insert(this.table).fields(Object.keys(object)).values(Object.values(object));
-        console.log(query.build());
+        let query = QueryBuilder().insert(this.table);
+        if (object.fields) {
+            query.fields(object.fields);
+        }
+        if (object.values) {
+            query.values(object.values);
+        }
+
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 
     async update(object) {
-        // Implement update method
-        let query = QueryBuilder.update(this.table).setValues(object);
-        console.log(query.build());
+        let query = QueryBuilder().update(this.table);
+        if(object.setValues){
+            query.setValues(object.setValues);
+        }
+        if(object.conditions){
+            query.conditions(object.conditions);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 
     async delete(object) {
         // Implement delete method
-        let condition = where().equal(Object.keys(object),Object.values(object));
-        let query = QueryBuilder.delete(this.table).where(object);
-        console.log(query.build());
+        let query = QueryBuilder().delete(this.table);
+        if (object.conditions){
+            query.conditions(object.conditions);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 }
--- src/repositories/enrollment_period.repository.js
+++ src/repositories/enrollment_period.repository.js
@@ -1,12 +1,15 @@
 import { where } from "../query-builder/condition.builder.js";
 import { QueryBuilder } from "../query-builder/query.builder.js";
 import { RepositoryStrategy, RepoStrategy } from "./repository-strategy/repository-strategy.js";
-
+import { connection } from "../configs/database.config.js";
 export class EnrollmentPeriodRepository extends RepositoryStrategy{
+
+    connection
 
     constructor(){
         super();
         this.table = RepoStrategy.ENROLLMENT_PERIOD;
+        this.connection = connection;
     }
 
     async find(object) {
@@ -21,43 +24,85 @@ export class EnrollmentPeriodRepository extends RepositoryStrategy{
         }
         if (object.joins){
             //Inners must be an array of objects
-            query.joinTable(object.joins);
+            object.joins.forEach(join => {
+                query.joinTable(join.table, join.type, join.field, join.fields, join.fields, join.fieldNameReference);
+            });
         }
         if (object.limit){
             //Limit must be a number
             query.limit(object.limit);
         }
-        if (object.limit && object.offset){
+        if (object.limit && object.offset >=0){
             //Offset must be a number
             query.offset(object.offset);
         }
 
-        query.build();
+        const [result] = await this.connection.execute(query.build.toString());
+
+        return result;
     }
 
     async findById(id, object) {
-        let condition = where().equal('id', id);
+        let condition = where().equal(`${this.table}.id`, id).build();
         let query = QueryBuilder().select(this.table).conditions(condition);
-        console.log(query.build());
-        // Implement findById method
+        if (object.fields !== undefined) {
+            //Fields must be an array of strings or a string
+            query.fields(object.fields);
+        }
+        if (object.conditions){
+            //Conditions must be an array of objects
+            query.conditions(object.conditions);
+        }
+        if (object.joins){
+            //Joins must be an array of objects
+            object.joins.forEach(join => {
+                query.joinTable(join.table, join.type, join.field, join.fields, join.fields, join.fieldNameReference);
+            });
+        }
+        if (object.limit){
+            //Limit must be a number
+            query.limit(object.limit);
+        }
+        if (object.limit && object.offset >= 0){
+            //Offset must be a number
+            query.offset(object.offset);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result[0];  // Devuelve el resultado de la ejecución de la query
+   
     }
 
     async create(object) {
-        // Implement create method
-        let query = QueryBuilder().insert(this.table).fields(Object.keys(object)).values(Object.values(object));
-        console.log(query.build().toString());
+        let query = QueryBuilder().insert(this.table);
+        if (object.fields) {
+            query.fields(object.fields);
+        }
+        if (object.values) {
+            query.values(object.values);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 
     async update(object) {
-        // Implement update method
-        let query = QueryBuilder.update(this.table).setValues(object);
-        console.log(query.build());
+        let query = QueryBuilder().update(this.table);
+        if(object.setValues){
+            query.setValues(object.setValues);
+        }
+        if(object.conditions){
+            query.conditions(object.conditions);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 
     async delete(object) {
         // Implement delete method
-        let condition = where().equal(Object.keys(object),Object.values(object));
-        let query = QueryBuilder.delete(this.table).where(object);
-        console.log(query.build());
+        let query = QueryBuilder().delete(this.table);
+        if (object.conditions){
+            query.conditions(object.conditions);
+        }
+        const [result] = await this.connection.execute(query.build().toString());
+        return result;  // Devuelve el resultado de la ejecución de la query
     }
 }
