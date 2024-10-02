56ddb17aa513bb748853741973157bae12980689
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-10-02 02:05:31 -0700

    create/update/delete
    (Almost Done)

--- src/repositories/subject-repository.js
+++ src/repositories/subject-repository.js
@@ -2,15 +2,19 @@ import { where } from "../query-builder/condition.builder";
 import { QueryBuilder } from "../query-builder/query.builder";
 import { RepositoryStrategy, RepoStrategy } from "./repository-strategy/repository-strategy";
 import { QueryBuilderException } from "../utils/exceptions/query-builder.exception";
-import { ConditionBuilderException } from "../utils/exceptions/condition-builder.exception";
+
+
 export class SubjectRepository extends RepositoryStrategy {
+    
     
     constructor() {
         super();
         this.table = RepoStrategy.SUBJECT;
+        
     }
 
-    async find(object) {
+
+    async find() {
         let query = QueryBuilder().select(this.table);
         try {
         query.build();  
@@ -41,51 +45,36 @@ export class SubjectRepository extends RepositoryStrategy {
     }
 
     async create(object) {
-        let query = QueryBuilder().insert(this.table);
-
-        if (object.fields) {
-            query.fields(object.fields);
-        }
-
-        if (object.values) {
-            query.values(object.values);
-        }
-
+        const fields = [`${this.table}.name`, `${this.table}.hours_per_week`, `${this.table}.semester`, `${this.table}.created_at`];
+        const values = [object.name, object.hoursPerWeek, object.semester, new Date().toISOString()];
+        let query = QueryBuilder().insert(this.table).fields(fields).values(values);
+    
         try {
             let result = query.build().toString();
             console.log(result);
         } catch (error) {
-            QueryBuilderException(error.message);
+            throw new QueryBuilderException(error.message);
         }
-        
     }
 
-    async update(object) {
-        let query = QueryBuilder().update(this.table);
-
-        if (object.fields) {
-            query.fields(object.fields);
-        }
-
-        if (object.values) {
-            query.values(object.values);
-        }
-
-        if (object.conditions) {
-            query.conditions(object.conditions);
-        }
-
+    async update(id, object) {
+        const condition = where().equal(`${this.table}.id`, id).build();
+        const fields = object.fields;
+        const values = object.values;
+    
+        let query = QueryBuilder().update(this.table).fields(fields).values(values).conditions(condition);
+    
         try {
             let result = query.build().toString();
             console.log(result);
-        }
-        catch (error) {
-            QueryBuilderException(error.message);
+        } catch (error) {
+            throw new QueryBuilderException(error.message);
         }
     }
 
-    async delete(object) {
-        let query = QueryBuilder().delete(this.table);
+    async delete(id, object) {
+        const condition = where().equal(`${this.table}.id`, id).build();
+        let query = QueryBuilder().delete(this.table).conditions(condition);
 
         if (object.conditions) {
             query.conditions(object.conditions);
