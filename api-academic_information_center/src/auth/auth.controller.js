64c309f739ebbbdfbde6e384995107691a2fa2d9
Daniel Armando Peña García <135689456+DanielPenaGarcia@users.noreply.github.com>
2024-10-20 16:59:19 -0700

    Tokens de respuesta
    Se implementa guard con bearer, y hay algunos paths publicos

--- api-academic_information_center/src/auth/auth.controller.js
+++ api-academic_information_center/src/auth/auth.controller.js
@@ -10,76 +10,68 @@ export class AuthController {
     this.sessionTimeout = 60;
   }
 
-  async logout(req, res) {
+  async getUser(req, res) {
+    const { academicId, token } = req.user;
+    const user = await this.authService.findUserByAcademicId({ academicId });
+    if (!user) {
+      return res.status(404).send({});
+    }
+    user.role = getUserRole(user);
+    user.token = token;
+    return res.status(200).send(user);
+  }
+
+  async deleteLogout(req, res) {
     res.clearCookie(TOKEN_COOKIE);
     return res.status(200).send({});
   }
 
-  async teacherLogin(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+  async postLoginTeacher(req, res) {
+    const { email, password } = req.body;
     const teacher = await this.authService.teacherLogin({ email, password });
     if (!teacher) {
-      return res.status(401).send("Invalid email or password");
+      return res.status(404).send("Teacher not found");
     }
-    const role = getUserRole(teacher);
     const token = this.#generateToken({
       academicId: teacher.academicId,
-      role,
-      secretKey: environment.secretTokenKey,
-      expiresIn: this.sessionTimeout,
+      role: getUserRole(teacher),
     });
-    res.cookie(TOKEN_COOKIE, token, {
-      httpOnly: true,
-    });
-    return res.status(200).send(teacher);
+    const data = { token: token, user: teacher };
+    return res.status(200).send(data);
   }
 
-  async studentLogin(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+  async postLoginStudent(req, res) {
+    const { email, password } = req.body;
     const student = await this.authService.studentLogin({ email, password });
     if (!student) {
-      return res.status(401).send("Invalid email or password");
+      return res.status(404).send("Student not found");
     }
-    const role = getUserRole(student);
     const token = this.#generateToken({
       academicId: student.academicId,
-      role,
-      secretKey: environment.secretTokenKey,
-      expiresIn: this.sessionTimeout,
+      role: getUserRole(student),
     });
-    res.cookie(TOKEN_COOKIE, token, {
-      httpOnly: true,
-    });
-    return res.status(200).send(student);
+    const data = { token: token, user: student };
+    return res.status(200).send(data);
   }
 
-  async administratorLogin(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+  async postLoginAdministrator(req, res) {
+    const { email, password } = req.body;
     const administrator = await this.authService.administratorLogin({
       email,
       password,
     });
     if (!administrator) {
-      return res.status(401).send("Invalid email or password");
+      return res.status(404).send("Administrator not found");
     }
-    const role = getUserRole(administrator);
     const token = this.#generateToken({
       academicId: administrator.academicId,
-      role,
-      secretKey: environment.secretTokenKey,
-      expiresIn: this.sessionTimeout,
+      role: getUserRole(administrator),
     });
-    res.cookie(TOKEN_COOKIE, token, {
-      httpOnly: true,
-    });
-    return res.status(200).send(administrator);
+    const data = { token: token, user: administrator };
+    return res.status(200).send(data);
   }
 
-  #generateToken({ academicId, role, secretKey, expiresIn }) {
-    return jwt.sign({ academicId, role }, secretKey, { expiresIn });
-}
-
+  #generateToken({ academicId, role, secretKey = environment.secretTokenKey, expiresIn = this.sessionTimeout }) {
+    return jwt.sign({ academicId, role }, secretKey, { expiresIn: expiresIn });
+  }
 }
--- api-academic_information_center/src/middlewares/guard.middleware.js
+++ api-academic_information_center/src/middlewares/guard.middleware.js
@@ -1,27 +1,35 @@
 import { environment } from "../environments/environment.js";
-import { TOKEN_COOKIE } from "../utils/constanst/token-cokie.constant.js";
 import jwt from "jsonwebtoken";
+import { TOKEN_COOKIE } from "../utils/constanst/token-cokie.constant.js";
+import { API_NAME } from "../utils/constanst/api-name.constant.js";
 
 const publicPaths = [
-  "auth/login/teachers",
-  "auth/login/students",
-  "auth/login/administrators",
-  "auth/logout",
-].map((path) => `/api/${path}`);
+  `/${API_NAME}/auth/login/teachers`,
+  `/${API_NAME}/auth/login/students`,
+  `/${API_NAME}/auth/login/administrators`,
+  `/${API_NAME}/auth/logout`,
+];
 
 export const guard = (req, res, next) => {
-  if (publicPaths.includes(req.path)) {
-    return next();
-  }
-  const token = req.cookies[TOKEN_COOKIE];
-  if (!token) {
-    return res.status(401).send("Unauthorized");
-  }
   try {
+      const path = req.path;
+    if (publicPaths.includes(path)) {
+      return next();
+    }
+    const token = bearerToken(req);
+    if (!token) {
+      return res.status(401).send("Unauthorized");
+    }
     const decoded = jwt.verify(token, environment.secretTokenKey);
     req.user = decoded;
+    req.user.token = token;
     next();
   } catch (error) {
     return res.status(401).send("Unauthorized");
   }
+};
+
+const bearerToken = (req) => {
+  const authorization = req.headers["authorization"];
+  return authorization.split(" ")[1];
 };
